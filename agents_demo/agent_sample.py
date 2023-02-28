0cf55de56e004d114f499acb61743a479dad706d
Sergey Kishkurno <s.kishkurno@gmail.com>
2023-02-28 16:07:20 +0200

    add agents demo

---
+++ agents_demo/agent_sample.py
@@ -0,0 +1,16 @@
+import faust
+
+
+app = faust.App("agents_demo")
+
+greetings_topic = app.topic(
+    "greetings",
+    value_type=str,
+    value_serializer="raw"
+)
+
+
+@app.agent(greetings_topic)
+async def greetings_processor(stream):
+    async for greeting in stream:
+        print(f"Greeting is '{greeting}'")
